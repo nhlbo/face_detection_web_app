2dee0cb39551764c18b4ad4700d62d9a990325b6
Hoang Long Nguyen <nhlbo@nhlbo.local>
2022-01-05 16:26:31 +0700

    add list object

--- detector/views.py
+++ detector/views.py
@@ -11,11 +11,12 @@ def index(request):
             img = form.cleaned_data['file']
             choice = form.cleaned_data['model']
             confidence_threshold = form.cleaned_data['threshold']
-            img, predicted_time = model.detect(img, confidence_threshold, choice)
+            img, predicted_time, result = model.detect(img, confidence_threshold, choice)
             return render(request, 'index.html', {
                 'form': form,
                 'src': img,
-                'predicted_time': predicted_time
+                'predicted_time': predicted_time,
+                'result': result
             })
     form = DetectForm()
     return render(request, 'index.html', {'form': form})
